c08fc9034a44df5e5deda41aec5ea5743334006b
greencrimea <tom.horton100@gmail.com>
2022-09-13 19:22:22 +1000

    .

---
+++ custom_env TEST/custom_env_TEST.py
@@ -0,0 +1,18 @@
+from gym import Env 
+from gym.spaces import Discrete, box
+import numpy as np
+import random
+
+class ShowerEnv(Env):
+
+    def __init__(self):
+        pass
+
+    def step(self):
+        pass
+
+    def render(self):
+        pass
+
+    def reset(self):
+        pass
